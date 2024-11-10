cd808ab6eda5511a46962ed7345fada738e18464
Austin Robertson <robertsonaustin27@gmail.com>
2024-11-10 18:27:08 -0500

    Initial display

---
+++ javascript.js
@@ -0,0 +1,46 @@
+function add(a, b)
+{
+    return a + b;
+}
+
+function subtract(a, b)
+{
+    return a - b;
+}
+
+function multiply(a, b)
+{
+    return a * b;
+}
+
+function divide(a, b)
+{
+    if (b == 0)
+    {
+        return "Das ist verboten. Try again.";
+    }
+    else
+    {
+        return a / b;
+    }
+}
+
+function operate(a, operator, b)
+{
+    if (operator == "+")
+    {
+        return add(a, b);
+    }
+    else if (operator == "-")
+    {
+        return subtract(a, b);
+    }
+    else if (operator == "x") // using x here as multiplcation sign
+    {
+        return multiply(a, b);
+    }
+    else if (operator == "/")
+    {
+        return divide(a, b);
+    }
+}
